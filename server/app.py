5e771d44dde152c3ac682c0450c8bca5826f2f91
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-10-15 12:29:14 +0300

    configured app to bookshub database, initialized migrate, imported SQLAlchemy instance from models.py

--- server/app.py
+++ server/app.py
@@ -1,12 +1,21 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from flask_migrate import Migrate
+from models import db
 
 # Create a Flask instance
 app = Flask(__name__)
 
+# Configuration for the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookshub.db'  
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize extensions
+db.init_app(app)
+migrate = Migrate(app, db)
+
 # Add extensions to use with flask
 api = Api(app)
-
 
 # Create Hello World Resource
 class HelloWorld(Resource):
