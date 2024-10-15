0eead019a9ca899d7277f5cc51e2827f1fbb2c9f
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-10-15 15:26:16 +0300

    adds root route and a root to GET all books

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify, request
 from flask_restful import Api, Resource
 from flask_migrate import Migrate
-from models import db
+from models import db, User, Book, Review, Transaction
 
 # Create a Flask instance
 app = Flask(__name__)
@@ -14,17 +14,21 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 migrate = Migrate(app, db)
 
-# Add extensions to use with flask
-api = Api(app)
+api = Api(app) # Add extensions to use with flask
 
-# Create Hello World Resource
-class HelloWorld(Resource):
+# GET /books: Retrieve all books.
+class Books(Resource):
 
     def get(self):
-        return {'hello': 'world'}
+        all_books = [book.to_dict() for book in Book.query.all()]
+        return make_response(jsonify(all_books), 200)
+
+api.add_resource(Books, '/books')
 
 
-api.add_resource(HelloWorld, '/')
+@app.route('/')
+def index():
+    return "<h1> Hello, Welcome to Books Swap Hub<h1>"
 
 if __name__ == '__main__':
     app.run(debug=True, port=5555)
