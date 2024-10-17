eb22145851fc4618850984258030fcc72b1bcea7
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-10-17 14:32:20 +0300

    imported home to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
 import "./App.css";
+import Home from "./pages/Home"
 
 function App() {
 
   return (
     <>
-      <p>Welcome to Book Swap Hub</p>
+      <Home />
     </>
   );
 }
