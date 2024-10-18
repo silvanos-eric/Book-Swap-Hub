487aef65f0eceddfdd2d931f01ba84f2fe32f082
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-10-18 22:09:19 +0300

    added Footer and Navbar to UserLogin page

--- client/src/pages/UserLogin.jsx
+++ client/src/pages/UserLogin.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
+import NavBar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const UserLogin = () => {
- 
   return (
-    <form>
-      <h2>Login</h2>
-      <input type="text" name="" id="username" />username
-      <input type="password" name="" id="password" />password
-    </form>
+    <div>
+      <NavBar />
+      <form>
+        <h2>Login</h2>
+        <input type="text" name="" id="username" />
+        username
+        <input type="password" name="" id="password" />
+        password
+      </form>
+      <Footer />
+    </div>
   );
 };
 
