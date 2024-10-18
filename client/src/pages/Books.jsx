20a19ce32cd0c7b852e84b1e516196e189b940e5
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-10-18 22:04:51 +0300

    added Footer and Navbar to Books page

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import BookCard from '../components/BookCard';
+import React, { useEffect, useState } from "react";
+import BookCard from "../components/BookCard";
+import NavBar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Books = () => {
-  
   return (
     <div>
+      <NavBar />
       <h2>Available Books</h2>
       <BookCard />
+      <Footer />
     </div>
   );
 };
