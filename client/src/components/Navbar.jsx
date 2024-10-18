c5793cd7c51bcccb6dc037a8d25d9572066880a9
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-10-18 22:07:13 +0300

    added navlinks to the Navbar componnent

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
-const Navbar = () => {
+function NavBar() {
   return (
     <nav>
-    {/* navlinks */}
+      <NavLink to="/" className="nav-link">
+        Home
+      </NavLink>
+      <NavLink to="/books" className="nav-link">
+        Books
+      </NavLink>
+      <NavLink to="/login" className="nav-link">
+        Login
+      </NavLink>
+      <NavLink to="/signup" className="nav-link">
+        Signup
+      </NavLink>
+      <NavLink to="/contact" className="nav-link">
+        Contacts
+      </NavLink>
     </nav>
   );
-};
+}
 
-export default Navbar;
+export default NavBar;
