2e7df80a9d300d47f9c5a7ef5ee9432e361ac2b2
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-10-19 22:29:02 +0300

    added feedback form and links to contact page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { FaGithub, FaLinkedin, FaEnvelope, FaWhatsapp } from "react-icons/fa";
+import {
+  FaGithub,
+  FaLinkedin,
+  FaEnvelope,
+  FaWhatsapp,
+  FaPhone,
+} from "react-icons/fa";
 import NavBar from "../components/Navbar";
 import Footer from "../components/Footer";
 
@@ -15,28 +21,73 @@ function Contact() {
   return (
     <div className="contact-container">
       <NavBar />
-      <h2>Contact Book Swap Hub</h2>
-      <h3>Get in Touch</h3>
-      <p>
-        We’d love to hear from you! If you have any questions about our book
-        exchange platform, need help, or want to provide feedback, feel free to
-        reach out via email:
-      </p>
-      <a href={mailtoLink} className="contact-email-link">
-        Send Us an Email
-      </a>
+      <section className="feedback_form">
+        <h2>Talk To Us</h2>
+        <label htmlFor="First Name">First Name</label>
+        <input type="text" name="first name" id="firstname" />
+        <label htmlFor="First Name">Last Name</label>
+        <input type="text" name="last name" id="lastname" />
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" />
+        <label htmlFor="Subject">Subject</label>
+        <input type="text" name="subject" id="subject" />
+        <textarea placeholder="Type your message here"></textarea>
+        <button>Send</button>
+      </section>
+      <section>
+        <h2>Connect With Us</h2>
+        <p>Let's connect via our social media:</p>
 
-      <div className="social-links">
-        <a href="https://github.com/bookswaphub">
-          <FaGithub /> GitHub
-        </a>
-        <a href="https://linkedin.com/in/bookswaphub">
-          <FaLinkedin /> LinkedIn
-        </a>
-        <a href="https://chat.whatsapp.com/HOAFbtTT34pLopYcn0oUGG">
-          <FaWhatsapp /> Whatsapp
-        </a>
-      </div>
+        <ul className="social-links">
+          <li>
+            <a
+              href="https://linkedin.com/in/bookswaphub"
+              aria-label="Visit OurLinkedIn profile"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin /> LinkedIn
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://chat.whatsapp.com/HOAFbtTT34pLopYcn0oUGG"
+              aria-label="Join our Whatsapp group"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaWhatsapp /> Whatsapp
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/bookswaphub"
+              aria-label="Visit our GitHub page"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub /> GitHub
+            </a>
+          </li>
+          <li>
+            <a
+              href={mailtoLink}
+              className="contact-email-link"
+              aria-label="Send us an email"
+            >
+              <FaEnvelope /> Email Us
+            </a>
+          </li>
+          <li>
+            <a
+              href="tel:+254750923269"
+              aria-label="Call or Text Us at +254750923269"
+            >
+              <FaPhone /> Call/Text
+            </a>
+          </li>
+        </ul>
+      </section>
       <Footer />
     </div>
   );
